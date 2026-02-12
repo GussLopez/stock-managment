3b10efd9d6067ba768d425913a40b886fb6cdf5b
Gustavo <gustavolopez819@gmail.com>
2026-02-12 13:48:08 -0500

    ADDED Admin Page into app router

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,14 @@
 export default function Admin() {
-  
+
   return (
-  <>
-  <span>Hello admin</span>
-  </>  
-  )  
+    <div>
+      <h1 className="my-2">Hello Admin</h1>
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+      </div>
+      <div className="bg-muted/50 min-h-screen flex-1 rounded-xl md:min-h-min" />
+    </div>
+  )
 }
