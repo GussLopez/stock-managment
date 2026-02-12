9abbf1d378a81c2fef2133745c71729a65689ba8
Gustavo <gustavolopez819@gmail.com>
2026-02-12 13:48:33 -0500

    CREATED admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { AuthContextProvider } from "@/context/AuthContext";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AdminSidebar } from "@/components/admin/admin-sidebar";
+import { Separator } from "@/components/ui/separator";
 
 export const metadata: Metadata = {
   title: "Stock Pro - Admin",
@@ -12,7 +15,23 @@ export default function AdminLayout({
 }>) {
   return (
     <AuthContextProvider>
-      {children}
+      <SidebarProvider>
+        <AdminSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator
+                orientation="vertical"
+                className="mr-2 data-[orientation=vertical]:h-4"
+              />
+            </div>
+          </header>
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            {children}
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
     </AuthContextProvider>
   );
 }
