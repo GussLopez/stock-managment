193c10bda46a163ed5959070ef97d31d60198e5b
Gustavo <gustavolopez819@gmail.com>
2026-02-16 12:00:19 -0500

    ADDED authentication check and redirect for Admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,13 @@
-export default function Admin() {
+import { createSupabaseServerClient } from "@/lib/supabase/server-client";
+import { redirect } from "next/navigation";
 
+export default async function AdminPage() {
+  const supabase = await createSupabaseServerClient();
+  const { data: { user } } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect('/auth/login')
+  }
   return (
     <div>
       <h1 className="my-2">Hello Admin</h1>
