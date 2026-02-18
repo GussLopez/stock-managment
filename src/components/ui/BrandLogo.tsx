757b0b9e939ede27bbbf09f4f86392827c85b306
Gustavo <gustavolopez819@gmail.com>
2026-02-18 11:27:00 -0500

    ADDED Brand Logo component

--- src/components/ui/BrandLogo.tsx
+++ src/components/ui/BrandLogo.tsx
@@ -1,11 +1,15 @@
 'use client'
 import { PackageIcon } from "@phosphor-icons/react"
 
-export const BrandLogo = () => { 
+interface BrandLogoProps {
+  variant?: 'white' | 'default'
+}
+
+export const BrandLogo = ({ variant = 'default' } : BrandLogoProps) => { 
   return (
-    <div className="flex items-center gap-1">
-      <PackageIcon size={40} weight="bold" className="text-primary"/>
-      <p className="text-3xl font-semibold">Stock<span className="text-neutral-500">Pro</span></p>
+    <div className={`flex items-center gap-1 ${variant === 'white' ? 'text-white' : ''} flex items-center gap-1`}>
+      <PackageIcon size={40} weight="bold" className={`${variant === 'white' ? 'text-white' : 'text-primary'}`}/>
+      <p className="text-3xl font-semibold">Stock<span className={`${variant === 'white' ? 'text-white' : 'text-neutral-500'}`}>Pro</span></p>
     </div>
   )
 }
