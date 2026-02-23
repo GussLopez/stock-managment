7aaa9cc4590cf3eda05651a920a50efc6af0ca2b
Gustavo <gustavolopez819@gmail.com>
2026-02-22 22:54:57 -0500

    ADDED ProductForm interface and updated index types export

---
+++ src/types/Product.ts
@@ -0,0 +1,10 @@
+export interface ProductForm {
+  name: string;
+  description: string;
+  price: number;
+  cost: number;
+  stock: number;
+  min_stock: number;
+  sku: string;
+  model: string;
+}
---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+export type { ProductForm } from './Product'
