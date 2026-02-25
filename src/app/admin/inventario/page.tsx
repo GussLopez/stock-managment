038a9992622234bb979aabc6f8b815d3863da869
Gustavo <gustavolopez819@gmail.com>
2026-02-25 14:31:15 -0500

    MODIFIED: Implemented tab navigation for inventory view and improved state management

--- src/app/admin/inventario/page.tsx
+++ src/app/admin/inventario/page.tsx
@@ -1,25 +1,52 @@
+'use client'
+import { Tabs, TabsContent, TabsContents, TabsList, TabsTrigger } from "@/components/animate-ui/components/animate/tabs";
 import AddProduct from "@/components/products/AddProduct";
 import ProductTable from "@/components/products/ProductTable";
+import { ListDashesIcon, SquaresFourIcon } from "@phosphor-icons/react";
 import { Box } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function InventarioPage() {
+  const [view, setView] = useState("table")
 
+  useEffect(() => {
+    const savedView = localStorage.getItem("inventory-view")
+    if (savedView) {
+      setView(savedView)
+    }
+  }, [])
+
+  const handleChange = (value: string) => {
+    setView(value)
+    localStorage.setItem("inventory-view", value)
+  }
   return (
     <div>
       <div className="flex justify-between">
         <div className="flex items-center gap-3">
-          <div>
-            <Box size={30} />
-          </div>
+          <Box size={30} />
           <h1 className="text-3xl font-semibold">Inventario</h1>
         </div>
+        <div className="flex items-center gap-4">
+          <Tabs value={view} onValueChange={handleChange}>
+            <TabsList>
+              <TabsTrigger value="table">
+                <ListDashesIcon />
+                Tabla
+              </TabsTrigger>
+              <TabsTrigger value="card">
+                <SquaresFourIcon />
+                Tarjetas
+              </TabsTrigger>
+            </TabsList>
+          </Tabs>
 
-        <div>
           <AddProduct />
         </div>
       </div>
-      <div>
-        <ProductTable />
+      <div className="mt-6">
+        {view === "table" && <ProductTable />}
+        {view === "card" && <p>Cards</p>}
       </div>
     </div>
   )
