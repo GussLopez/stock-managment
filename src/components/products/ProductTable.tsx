88aa55eaad2b4fbdfe5e9a18c79f55cbe9c3659e
Gustavo <gustavolopez819@gmail.com>
2026-02-25 19:08:17 -0500

    MODIFIED: Refactored ProductTable component to accept props for product data and loading state

--- src/components/products/ProductTable.tsx
+++ src/components/products/ProductTable.tsx
@@ -1,6 +1,5 @@
 'use client';
-import { getProducts } from "@/lib/services/productService";
-import { useQuery } from "@tanstack/react-query";
+
 import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "../ui/table";
 import { Image, MoreHorizontal, PencilIcon, SlidersHorizontal, TrashIcon } from "lucide-react";
 import { Skeleton } from "../ui/skeleton";
@@ -10,23 +9,23 @@ import { Button } from "../ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import EditProductModal from "./EditProductModal";
 
-export default function ProductTable() {
+interface ProductTableProps {
+  data: {
+    id: string;
+    name: string;
+    price: number;
+    stock: number;
+    image: string | null;
+  }[]
+  isLoading: boolean;
+  totalInventario: number
+}
+
+export default function ProductTable({ data, isLoading, totalInventario }: ProductTableProps) {
   const [open, setOpen] = useState(false);
   const [openEditModal, setOpenEditModal] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState<string | null>(null)
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["StockProducts"],
-    queryFn: async () => {
-      const data = await getProducts();
-      return data;
-    },
-    retry: 1,
-    refetchOnWindowFocus: false
-  })
 
-  const totalInventario = data?.reduce((acc, product) => {
-    return acc + (product.price * product.stock);
-  }, 0) || 0;
 
   return (
     <div className="rounded-md border  mt-6">
