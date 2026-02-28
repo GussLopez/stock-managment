f86a7f5c299c4007a6799feaa356e0bc1a14762d
Gustavo <gustavolopez819@gmail.com>
2026-02-28 00:04:40 -0500

    MODIFIED product card & deleted product form

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -1,26 +1,24 @@
-import { Image, MoreVertical, PencilIcon, SlidersHorizontal } from "lucide-react";
+import { Package, MoreVertical, PencilIcon, SlidersHorizontal, Trash2Icon } from "lucide-react";
 import { Badge } from "../ui/badge";
 import { Button } from "../ui/button";
-import { NotePencilIcon, TrashIcon } from "@phosphor-icons/react";
 import { Separator } from "../ui/separator";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { Product } from "@/types";
 
 interface ProductCardProps {
-  product: {
-    id: string;
-    name: string;
-    price: number;
-    stock: number;
-    sku: string;
-    cost: number;
-    image: string | null;
-  }
+  product: Product;
+  onEdit: (product: Product) => void;
+  onDelete: (product: Product) => void
+  onView: (product: Product) => void
 }
 
-export default function ProductCard({ product }: ProductCardProps) {
+export default function ProductCard({ product, onEdit, onDelete, onView }: ProductCardProps) {
+  const stockColor = product.stock <= product.min_stock
+    ? "text-red-500 bg-red-50 dark:bg-red-950/30"
+    : "text-emerald-600 bg-emerald-50 dark:bg-emerald-950/30";
   return (
-    <div className="rounded-lg border overflow-hidden">
-      <div className="h-45 flex flex-col justify-center items-center bg-facent text-neutral-300 dark:text-neutral-600 relative">
+    <div className="rounded-lg border border-muted overflow-hidden">
+      <div className="h-45 flex flex-col justify-center items-center bg-facent text-neutral-400 dark:text-neutral-600 relative">
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button
@@ -35,7 +33,7 @@ export default function ProductCard({ product }: ProductCardProps) {
           <DropdownMenuContent align="end">
             <DropdownMenuGroup>
               <DropdownMenuLabel className="text-xs">Acciones</DropdownMenuLabel>
-              <DropdownMenuItem>
+              <DropdownMenuItem onClick={() => onEdit(product)}>
                 <PencilIcon />
                 Editar
               </DropdownMenuItem>
@@ -45,47 +43,67 @@ export default function ProductCard({ product }: ProductCardProps) {
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuGroup>
+            <DropdownMenuGroup onClick={() => onDelete(product)}>
               <DropdownMenuItem variant="destructive">
-                <TrashIcon />
+                <Trash2Icon />
                 Eliminar
               </DropdownMenuItem>
             </DropdownMenuGroup>
           </DropdownMenuContent>
         </DropdownMenu>
         {product?.image ? (
-          <img src={product.image} alt={`imagen de ${product.name}`} />
+          <div
+            className="w-full h-full flex items-center justify-center p-3 cursor-pointer"
+            onClick={() => onView(product)}
+          >
+            <img
+              src={product.image}
+              alt={product.name}
+              className="max-w-full max-h-full object-contain"
+            />
+          </div>
         ) : (
-          <>
-            <Image className="size-10" />
-            <span className="text-xs">Sin imagen</span>
-          </>
+          <div
+            className="flex flex-col items-center gap-2 cursor-pointer"
+            onClick={() => onView(product)}
+          >
+            <Package className="size-10 stroke-[1.5] text- opacity-60" />
+            <span className="text-[10px] uppercase tracking-widest font-semibold opacity-60">Sin imagen</span>
+          </div>
         )}
       </div>
       <div className="p-3">
-        <div>
-          <p className="font-semibold">{product.name}</p>
-          <Badge variant={'outline'} className="font-mono text-xs rounded">{product.sku}</Badge>
+        <div className="flex justify-between items-start mb-3">
+          <div>
+            <h3 className="font-bold text-base leading-tight text-foreground/90">{product.name}</h3>
+            <Badge variant={'outline'} className="mt-1.5 font-mono text-[11px] text-primary-light font-semibold">
+              {product.sku}
+            </Badge>
+          </div>
         </div>
-        <div className="flex items-center py-1 px-4 mt-3 border rounded-md">
+        <div className="flex items-center py-2 px-3 bg-muted/30 rounded-lg border border-muted/50">
           <div className="flex-1">
-            <p className="text-xs font-semibold text-muted-foreground">Costo</p>
-            <p className="mt-0.5 text-sm font-semibold">
-              $ {product.cost.toLocaleString()}
-            </p>
+            <p className="text-[10px] uppercase tracking-wider font-bold text-muted-foreground/70">Costo</p>
+            <p className="text-sm font-semibold text-foreground/80">${product.cost.toLocaleString()}</p>
           </div>
-          <Separator orientation="vertical" className="mx-4 h-8 w-[0.5px]" />
-          <div className="flex-1 text-right">
-            <p className="text-xs font-semibold text-muted-foreground">Precio venta</p>
-            <p className="mt-0.5 text-sm font-semibold">
-              $ {product.price.toLocaleString()}
-            </p>
+          <Separator orientation="vertical" className="h-7" />
+          <div className="flex-1 pl-4 text-end">
+            <p className="text-[10px] uppercase tracking-wider font-bold text-muted-foreground/70">Venta</p>
+            <p className="text-sm font-bold text-primary-light">${product.price.toLocaleString()}</p>
           </div>
         </div>
-        <Separator className="my-4 h-[0.5px]" />
-        <div className="flex justify-between">
-          <p className="font-bold text-lg">$ {(product.price * product.stock).toLocaleString()} MXN</p>
-          <p className="text-sm text-green-600 dark:text-green-400 font-bold">{product.stock} un.</p>
+        <div className="mt-4 pt-3 border-t flex justify-between items-end">
+          <div>
+            <p className="text-[10px] font-medium text-muted-foreground mb-0.5">Valor Total</p>
+            <p className="font-black text-xl tracking-tight leading-none text-foreground">
+              ${(product.price * product.stock).toLocaleString()}
+              <span className="text-[10px] ml-1 text-muted-foreground font-normal">MXN</span>
+            </p>
+          </div>
+
+          <div className={`px-2 py-1 rounded-md text-xs font-bold flex items-center gap-1 ${stockColor}`}>
+            {product.stock} <span className="text-[10px] font-medium opacity-80 uppercase">un.</span>
+          </div>
         </div>
 
       </div>
