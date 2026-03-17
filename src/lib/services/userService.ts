643e3532b9cb0b580a5da1a7c00d4ac1c8da6139
Gustavo <gustavolopez819@gmail.com>
2026-03-17 12:18:48 -0500

    ADDED getProfileById function

--- src/lib/services/userService.ts
+++ src/lib/services/userService.ts
@@ -31,3 +31,15 @@ export async function deleteProfile(profileId: string) {
     
   if (error) throw error;
 }
+
+export async function getProfileById(id: string) {
+  const { data, error } = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("id", id)
+    .single()
+
+  if (error) throw error;
+
+  return data;
+}
