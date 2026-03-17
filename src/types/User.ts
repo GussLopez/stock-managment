22b2d8045210599a416b363a5609526a67a323f3
Gustavo <gustavolopez819@gmail.com>
2026-03-17 12:16:50 -0500

    ADDED Profile type

--- src/types/User.ts
+++ src/types/User.ts
@@ -5,3 +5,12 @@ export interface Employe {
   role: string;
   business_id: string;
 }
+
+export interface Profile {
+  business_id: string;
+  created_at: string | null;
+  email: string;
+  full_name: string | null;
+  id: string;
+  role: string;
+}
