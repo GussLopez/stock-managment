9d268abbe57f1078f799396babceff207aafcb05
Gustavo <gustavolopez819@gmail.com>
2026-03-28 01:21:37 -0500

    ADDED subconsults

--- src/lib/services/movementService.ts
+++ src/lib/services/movementService.ts
@@ -6,19 +6,38 @@ export interface MovementInsert {
   product_id: string;
   business_id: string;
   quantity: number;
-  type: 'entrada' | 'salida' | 'ajuste';
+  type: "entrada" | "salida" | "ajuste";
   user_id?: string | null;
   supplier_id?: number | null;
   reference?: string | null;
   batch_id: string;
 }
 
-export async function createMovement (movements: MovementInsert[]) {
+export async function createMovement(movements: MovementInsert[]) {
   const { error } = await supabase
-    .from('inventory_movements')
-    .insert(movements)
+    .from("inventory_movements")
+    .insert(movements);
 
   if (error) throw error;
 
-  return { success: true }
+  return { success: true };
+}
+
+export async function getMovements() {
+  const { data, error } = await supabase
+    .from("inventory_movements")
+    .select(`
+        *,
+        profiles (
+          full_name
+        ),
+        products (
+          name,
+          sku
+        )
+      `);
+
+  if (error) throw error;
+
+  return data;
 }
