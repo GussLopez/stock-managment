db8e4187451af8ecdd629fcfb9e543d19b216cd1
Gustavo <gustavolopez819@gmail.com>
2026-03-28 01:21:59 -0500

    ADDED tabls for movements history & register

--- src/app/admin/entradas-salidas/page.tsx
+++ src/app/admin/entradas-salidas/page.tsx
@@ -1,299 +1,41 @@
 'use client'
-import AddSupplierDialog from "@/components/admin/suppliers/AddSupplierDialog";
-import InputQuantity from "@/components/sales/InputQuantity";
-import SearchProductInput from "@/components/sales/SearchProductInput";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Separator } from "@/components/ui/separator";
-import { Spinner } from "@/components/ui/spinner";
-import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { Textarea } from "@/components/ui/textarea";
-import { createMovement, MovementInsert } from "@/lib/services/movementService";
-import { getBusinessSuppliers } from "@/lib/services/supplierService";
-import { useBusinessStore } from "@/store/BusinessStore";
-import { useUserStore } from "@/store/UserStore";
-import { Product } from "@/types";
-import { useQuery } from "@tanstack/react-query";
-import { ArrowLeft, ArrowRightLeft, Plus, SquarePen, Trash } from "lucide-react";
-import { useEffect, useState } from "react"
-import { NumberField } from "react-aria-components";
-import { sileo } from "sileo";
 
-interface ItemsProps {
-  tempId: string;
-  product_id: string;
-  product_name: string;
-  product_sku: string;
-  quantity: number;
-}
+import { Tabs, TabsList, TabsTrigger } from "@/components/animate-ui/components/animate/tabs";
+import MovementsHistory from "@/components/movements/MovementsHistory";
+import RegisterMovement from "@/components/movements/RegisterMovement";
+import { ArrowRightLeft, FolderInput, History } from "lucide-react";
+import { useState } from "react";
 
 export default function EntradasPage() {
-  const [items, setItems] = useState<ItemsProps[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
-  const [notes, setNotes] = useState('');
-  const [popover, setPopover] = useState(false);
-  const [suppModal, setSuppModal] = useState(false);
+  const [view, setView] = useState("register")
 
-  const [supplierId, setSupplierId] = useState<number | null>(null)
-  const userId = useUserStore((state) => state.id);
-  const businessId = useBusinessStore((state) => state.id);
-
-  const addItem = (product: Product) => {
-    setItems([
-      ...items,
-      {
-        tempId: crypto.randomUUID(),
-        product_id: product.id || '',
-        product_name: product.name || '',
-        product_sku: product.sku || '',
-        quantity: 1,
-      }
-    ])
-  }
-
-  useEffect(() => {
-    if (selectedProduct) {
-      addItem(selectedProduct);
-      setSelectedProduct(null);
-    }
-  }, [selectedProduct])
-
-  const updateQuantity = (tempId: string, newQuantity: number) => {
-    setItems(prev =>
-      prev.map(item =>
-        item.tempId === tempId
-          ? { ...item, quantity: newQuantity }
-          : item
-      )
-    )
-  }
-
-  const deleteItem = (tempId: string) => {
-    setItems(prev =>
-      prev.filter(item => item.tempId !== tempId)
-    )
-  }
-
-  const validateNotes = () => {
-    if (notes.length === 0) return;
-    if (notes.length < 8) {
-      sileo.warning({
-        title: 'La nota debe ser más larga',
-        description: 'La nota debe ser mayor a 8 caracteres'
-      })
-      setNotes('')
-    }
-
-    setPopover(false);
-  }
-  const handleSave = async () => {
-    if (items.length === 0) return;
-    setLoading(true);
-    try {
-      const operationId = crypto.randomUUID();
-      const movementsToSave: MovementInsert[] = items.map(item => ({
-        product_id: item.product_id,
-        business_id: businessId || '',
-        user_id: userId,
-        quantity: item.quantity,
-        type: 'entrada',
-        reference: notes,
-        supplier_id: supplierId,
-        batch_id: operationId
-      }))
-
-      await createMovement(movementsToSave);
-
-      sileo.success({
-        title: 'Movimientos registrados',
-        description: 'Los movimientos se registraron correctamente',
-        autopilot: false
-      })
-      setItems([]);
-    } catch (error) {
-      console.error(error);
-      sileo.error({
-        title: 'Error al registrar los movimientos',
-        description: 'Hubo un error al registrar los movimientos, por favor intente más tarde'
-      })
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  const { data, isLoading } = useQuery({
-    queryKey: ["business-suppliers"],
-    queryFn: async () => {
-      const data = await getBusinessSuppliers()
-      return data;
-    },
-    retry: 1
-  })
   return (
     <div>
-      <div className="flex items-center gap-3">
-        <ArrowRightLeft size={30} />
-        <h1 className="text-3xl font-semibold">Entradas / Salidas</h1>
-      </div>
-
-      <div className="mt-10 flex gap-4">
-        <div className="max-w-sm w-full">
-          <div className="flex gap-2">
-            <Select
-              value={supplierId?.toString() || 'ninguno'}
-              onValueChange={(value) => setSupplierId(Number(value))}
-            >
-              <SelectTrigger className="w-full">
-                <SelectValue />
-              </SelectTrigger>
-              <SelectContent position="popper">
-                <SelectGroup>
-                  <SelectLabel>Proveedores</SelectLabel>
-                  <SelectItem value="ninguno" defaultChecked>Ninguno</SelectItem>
-                  {isLoading && <p className="p-2 text-sm  text-muted-foreground">Cargando...</p>}
-                  {data?.length === 0 ? (
-                    <p className="p-2 text-sm  text-muted-foreground">No hay proveedores registrados</p>
-                  ) : (
-                    data?.map((supp) => (
-                      <SelectItem key={supp.id} value={supp.id.toString()}>{supp.name}</SelectItem>
-                    ))
-                  )}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-            <Button
-              size={'icon'}
-              variant={'outline'}
-              onClick={() => setSuppModal(true)}
-            >
-              <Plus size={20} />
-            </Button>
-          </div>
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-3">
+          <ArrowRightLeft size={30} />
+          <h1 className="text-3xl font-semibold">Entradas / Salidas</h1>
         </div>
-        <div>
-          <Popover open={popover} onOpenChange={setPopover}>
-            <PopoverTrigger asChild>
-              <Button variant={'outline'}>
-                <SquarePen size={20} className="text-green-600" />
-                Agregar Nota
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent align="start" className="w-85 p-0">
-              <Textarea
-                placeholder="Detalles adicionales..."
-                className="min-h-20 max-h-50 p-4 focus-visible:ring-0 border-0  shadow-none"
-                onChange={e => setNotes(e.target.value)}
-                value={notes}
-              />
-              <Separator className="w-full h-px" />
-              <div className="flex items-center justify-between px-3 py-2">
-                <Button
-                  variant={'ghost'}
-                  size={'icon-sm'}
-                  onClick={() => setPopover(false)}
-                >
-                  <ArrowLeft size={20} />
-                  <span className="sr-only">Cerrar menu de notas</span>
-                </Button>
-                <Button
-                  size={'sm'}
-                  variant={'outline'}
-                  onClick={validateNotes}
-                >
-                  Guardar
-                </Button>
-              </div>
-            </PopoverContent>
-          </Popover>
+        <div className="flex items-center gap-4">
+          <Tabs value={view} onValueChange={setView}>
+            <TabsList>
+              <TabsTrigger value="register">
+                <FolderInput />
+                Registrar
+              </TabsTrigger>
+              <TabsTrigger value="record">
+                <History />
+                Historial
+              </TabsTrigger>
+            </TabsList>
+          </Tabs>
         </div>
       </div>
-      <div className="mt-4 border border-input rounded-lg overflow-hidden">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Producto</TableHead>
-              <TableHead>SKU</TableHead>
-              <TableHead>Cantidad</TableHead>
-              <TableHead className="text-end">Acciones</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {items.map((item, i) => (
-              <TableRow key={i}>
-                <TableCell className="text-lg font-semibold">
-                  {item.product_name}
-                </TableCell>
-                <TableCell>
-                  <Badge variant={'secondary'} className="font-mono">{item.product_sku}</Badge>
-                </TableCell>
-                <TableCell className="max-w-15 overflow-hidden">
-                  <NumberField
-                    aria-label="Cantidad del producto"
-                    value={item.quantity}
-                    minValue={1}
-                    maxValue={999}
-                    onChange={(value) =>
-                      updateQuantity(item.tempId, value)
-                    }
-                    className='w-full max-w-xs space-y-2'
-                  >
-                    <InputQuantity />
-                  </NumberField>
-
-                </TableCell>
-                <TableCell className="text-end">
-                  <Button
-                    variant={'ghost'}
-                    className="bg-red-600/10 text-red-500"
-                    size={'icon-sm'}
-                    onClick={() => deleteItem(item.tempId)}
-                  >
-                    <Trash size={20} />
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-            <TableRow>
-              <TableCell colSpan={5}>
-                <SearchProductInput
-                  setProduct={setSelectedProduct}
-                  btnClass="w-full max-w-fit justify-between"
-                  btnSize="sm"
-                  placeholder="Seleccionar producto"
-                />
-              </TableCell>
-            </TableRow>
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TableCell colSpan={5} className="text-end">
-                <Button
-                  disabled={items.length === 0 || loading}
-                  onClick={handleSave}
-                >
-                  {loading ? (
-                    <>
-                      <Spinner />
-                      Registrando
-                    </>
-                  ) : 'Registrar Movimiento'}
-                </Button>
-              </TableCell>
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </div>
-
-      <AddSupplierDialog
-        open={suppModal}
-        onClose={() => setSuppModal(false)}
-        onCraeted={(supplier) => {
-          setSupplierId(supplier.id)
-        }}
-      />
+      {view === 'register' ? (
+        <RegisterMovement />
+      ) : (
+        <MovementsHistory />
+      )}
     </div >
 
   )
---
+++ src/components/movements/MovementsHistory.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import { Button } from "../ui/button"
+import { RangeDatePicker } from "../ui/range-date"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table"
+import { DownloadSimpleIcon, FileTextIcon } from "@phosphor-icons/react"
+import { useQuery } from "@tanstack/react-query"
+import { getMovements } from "@/lib/services/movementService"
+import { Badge } from "../ui/badge"
+import { Trash, Trash2 } from "lucide-react"
+
+export default function MovementsHistory() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["business-movements"],
+    queryFn: async () => {
+      const data = await getMovements();
+      return data;
+    },
+    retry: 1
+  })
+
+  console.log(data);
+  return (
+    <div>
+      <div className="flex justify-between items-center mt-6">
+        <RangeDatePicker />
+        <div className="flex gap-2">
+          <Button
+            variant={'outline'}
+            disabled={data?.length === 0}
+          >
+            <FileTextIcon size={20} weight="bold" />
+            PDF Lista
+          </Button>
+          <Button
+            variant={'outline'}
+            disabled={data?.length === 0}
+          >
+            <DownloadSimpleIcon size={20} weight="bold" />
+            CSV
+          </Button>
+        </div>
+      </div>
+      <div className="mt-5 border border-input rounded-lg overflow-hidden">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Fecha</TableHead>
+              <TableHead>Usuario</TableHead>
+              <TableHead>Tipo</TableHead>
+              <TableHead>Producto</TableHead>
+              <TableHead>Cantidad</TableHead>
+              <TableHead>Notas</TableHead>
+              <TableHead className="text-end">Acciones</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data?.map((movement) => {
+              const date = new Date(movement.created_at)
+              return (
+                <TableRow key={movement.id}>
+                  <TableCell>{date.toLocaleDateString()}</TableCell>
+                  <TableCell>{movement.profiles?.full_name}</TableCell>
+                  <TableCell>
+                    <Badge className="capitalize">{movement.type}</Badge>
+                  </TableCell>
+                  <TableCell className="truncate">
+                    {movement.products.sku} - {movement.products.name}
+                  </TableCell>
+                  <TableCell className="font-semibold">{movement.quantity}</TableCell>
+                  <TableCell className="truncate max-w-20 text-muted-foreground">{movement.reference || '-'}</TableCell>
+                  <TableCell className="text-end">
+                    <Button
+                      size={'icon-sm'}
+                      variant={'ghost'}
+                      className="text-red-500 bg-red-600/10 hover:text-red-600 hover:bg-red-500/20 dark:text-red-400 dark:hover:bg-red-600/30"
+                    >
+                      <Trash2 />
+                      <span className="sr-only">Eliminar registro</span>
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    </div >
+  )
+}
